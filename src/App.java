70249edfff154058cd71244158711aa83df33cef
gsTavares <gustavo.tavares24@outlook.com>
2023-10-15 18:17:17 -0300

    feat: Function<T,R> example

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -8,6 +9,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         runConsumerExample();
         runSupplierExample();
+        runFunctionExample();
     }
 
     /*
@@ -37,5 +39,16 @@ public class App {
                 .limit(5)
                 .toList();
         greetingsList.forEach(System.out::println);
+    }
+
+    /*
+     * Function<T, R>
+     * Receives a T argument and returns the R argument. Used object transforming or mapping
+     */
+    static void runFunctionExample() {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
+        Function<Integer, Integer> multiplyBy2 = number -> number * 2;
+        List<Integer> doubledValues = numbers.stream().map(multiplyBy2).toList();
+        doubledValues.forEach(System.out::println);
     }
 }
